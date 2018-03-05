870cbf623aa70c0e58f5d83c6c61d4b823cb10c9
john <rod_john@163.com>
2018-03-05 12:25:40 +0800

    swagger

---
+++ 50_spring-boot/410_swagger/src/main/java/com/john/rod/swagger/Application.java
@@ -0,0 +1,18 @@
+package com.john.rod.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+
+
+}
